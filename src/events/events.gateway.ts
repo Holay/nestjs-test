dd5b2454a8fc4ffc0545da1227eb05f8b178ecf0
Mateo Penagos <mateofer_2@hotmail.com>
2021-05-14 11:22:20 -0500

    test method

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,5 +1,6 @@
 import {
   MessageBody,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
@@ -10,9 +11,15 @@ import { map } from 'rxjs/operators';
 import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class EventsGateway {
+export class EventsGateway implements OnGatewayInit {
   @WebSocketServer()
   server: Server;
+
+  afterInit(server: any) {
+    setInterval(() => {
+      this.sendData('PENAM092')
+    }, 30000)
+  }
 
   @SubscribeMessage('events')
   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
@@ -22,5 +29,14 @@ export class EventsGateway {
   @SubscribeMessage('identity')
   async identity(@MessageBody() data: number): Promise<number> {
     return data;
+  }
+
+  @SubscribeMessage('test')
+  replyToUser(@MessageBody() user: string){
+    this.sendData(user)
+  }
+
+  sendData(user) {
+    this.server.emit(`Notifications-${user}`, { messages: [`Test notification sent to: ${user}`], type: 'success', previewMessage: 'Test Notification', header: 'Test', time: new Date() })
   }
 }
