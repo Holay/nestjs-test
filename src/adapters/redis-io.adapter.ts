18acc420825fbfad0ffc2cfa4f84be74ef5160b5
Mateo Penagos <mateofer_2@hotmail.com>
2021-05-14 15:21:30 -0500

    added adapter again again

--- src/adapters/redis-io.adapter.ts
+++ src/adapters/redis-io.adapter.ts
@@ -2,7 +2,7 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import * as redisIoAdapter from 'socket.io-redis';
 
-const redisAdapter = redisIoAdapter({ host: 'localhost', port: 6379 });
+const redisAdapter = redisIoAdapter({ host: 'redis://redistogo:e8df38aa21d500b533b61a925f75a238@scat.redistogo.com', port: 10732 });
 
 export class RedisIoAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions): any {
