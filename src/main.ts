a91c5bf87990fb490b7960b7b1a289538f52bb85
Mateo Penagos <mateofer_2@hotmail.com>
2021-05-14 14:34:23 -0500

    redis adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { RedisIoAdapter } from './adapters/redis-io.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new RedisIoAdapter(app))
   await app.listen(process.env.PORT || 3000);
   app.enableCors({ origin: '*' });
   console.log(`Application is running on: ${await app.getUrl()}`);
